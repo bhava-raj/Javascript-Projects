570b41418b2e642c4c4b48c5c442e443343ad91b
Bhava-raj <bhava.raj.200515@gmail.com>
2025-08-29 22:14:20 +0530

    Make Calorie Counter a normal folder inside repo

---
+++ Randombg/app.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 5; i++) {
+  console.log("Hello " + i);
+}
+function greet(name) {
+  return "Hello " + name;
+}
+console.log(greet("Ace")); // Hello Ace
+
+
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("text").innerText = "Button clicked!";
+  });
+
+
+
